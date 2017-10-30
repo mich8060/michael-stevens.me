6fb19e494e2ec5205780fcae51265cfe79ee352c
Michael Stevens <mich8060@hotmail.com>
2017-10-30 13:44:30 -0600

    Staging config

---
+++ wp-config.staging.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Production environment config settings
+ *
+ * Enter any WordPress config settings that are specific to this environment 
+ * in this file.
+ * 
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    2.0.0
+ * @author     Studio 24 Ltd  <hello@studio24.net>
+ */
+  
+
+// ** MySQL settings - You can get this info from your web host ** //    
+$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
+
+/** The name of the database for WordPress */
+define('DB_NAME', trim($url['path'], '/'));
+
+/** MySQL database username */
+define('DB_USER', $url['user']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $url['pass']);
+
+/** MySQL hostname */
+define('DB_HOST', $url['host']);
+
+/** MySQL database password - set in wp-config.local.php */
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
